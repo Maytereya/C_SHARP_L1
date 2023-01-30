3514120cca8038996ac86628fa3a2a653f5e52ed
Vladimir <rakhmanov-v@ya.ru>
2023-01-30 21:33:32 +0400

    Initial commit for Lesson 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+//int m = int.Parse(Console.ReadLine()); // выгоднее с точки зрения использования памяти. тк использует ту же ячейку памяти.
+//string? name = Console.ReadLine();
+
+Console.WriteLine($"Результат: {n*5}");
+
+
